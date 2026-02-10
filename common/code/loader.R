88ec589373f9de35c74bef4050bea3f428011bc1
Gio <gio@iastate.edu>
2026-02-10 00:13:50 -0600

    Update file path
    Changed file paths for common function, it will read locally if run on gio's machine, ortherwise reads from github

--- common/code/loader.R
+++ common/code/loader.R
@@ -21,9 +21,15 @@ library(sf)
 
 # Load R scripts ---------------------------------------------------------------
 
+if (Sys.getenv("USERNAME") == "gio") {
+  common.function.path <- ".."
+} else {
+  common.function.path <- "https://raw.githubusercontent.com/I2D2-Data-Team/IDD_Dashboard_App_Dev/refs/heads/main"
+}
+
 # Read common functions
-source("https://raw.githubusercontent.com/I2D2-Data-Team/IDD_Dashboard_App_Dev/refs/heads/main/common/code/functions.R")
+source(file.path(common.function.path, "common/code/functions.R"))
 # Read geospatial data and color pallets
-source("https://raw.githubusercontent.com/I2D2-Data-Team/IDD_Dashboard_App_Dev/refs/heads/main/common/code/global.R")
+source(file.path(common.function.path, "common/code/global.R"))
 # Read modules
-source("https://raw.githubusercontent.com/I2D2-Data-Team/IDD_Dashboard_App_Dev/refs/heads/main/common/code/modules.R")
+source(file.path(common.function.path, "common/code/modules.R"))
